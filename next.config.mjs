ff48f04becd592fecbe6cce364dce80449107948
ishan <ishannagpal554@gmail.com>
2024-09-16 18:07:07 -0500

    !

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {  
+const nextConfig = {
     webpack(config) {
-    config.experiments = {
-      asyncWebAssembly: true,
-    };
-
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: "webassembly/async",
-    });
-
-    return config;
-  },}
-
-export default nextConfig
+      config.experiments = {
+        asyncWebAssembly: true,
+        layers: true,
+      };
+  
+      config.module.rules.push({
+        test: /\.wasm$/,
+        type: "webassembly/async",
+      });
+  
+      return config;
+    },
+  }
+  
+  export default nextConfig;
+  
