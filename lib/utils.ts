0d794448da705dac57a44a76769dc72c871ea62d
ishan <ishannagpal554@gmail.com>
2024-11-30 10:33:20 -0600

    switched uuid

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,6 +7,7 @@ import {
 } from 'ai'
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { v4 as uuidv4 } from 'uuid'
 
 export interface User {
     id: string
@@ -58,16 +59,8 @@ export function getLocalStorage(key: string) {
 }
 
 export function generateUUID(): string {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
-        /[xy]/g,
-        function (c) {
-            const r = (Math.random() * 16) | 0
-            const v = c === 'x' ? r : (r & 0x3) | 0x8
-            return v.toString(16)
-        }
-    )
+    return uuidv4();
 }
-
 function addToolMessageToChat({
     toolMessage,
     messages,
